89678012dc37beb08b87f22ec1cfbcad0d53d5aa
Alvaro Placido <164355474+AlvaroP2003@users.noreply.github.com>
2025-07-22 16:14:02 +0200

    Added listings sections

--- src/Pages/Listings.jsx
+++ src/Pages/Listings.jsx
@@ -3,9 +3,16 @@ import { Hourglass } from "lucide-react"
 
 export default function Listings() {
     return (
-        <section className="flex flex-col justify-center items-center gap-2.5 h-[90vh] lg:h-[85vh]">
-            <Hourglass size={100} strokeWidth={1} className="text-gray-400"/>
-            <h1 className="text-lg  text-gray-400">FEATURE COMING SOON</h1>
+        <section className="h-[90vh] lg:h-[85vh]">
+
+           <div className="relative flex items-center justify-center h-[250px] border-1">
+                <h1>Find your perfect place to call home</h1>
+
+                <div className="absolute bottom-0 flex ">
+                    <button className="bg-[#C9B99F] py-2.5 px-5 ">To Rent</button>
+                    <button>To Buy</button>
+                </div>
+           </div>
         </section>
     )
 }
